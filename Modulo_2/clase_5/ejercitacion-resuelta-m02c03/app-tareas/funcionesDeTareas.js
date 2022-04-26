6cc64b18bb6e48ba7f0e143286b3c85c671dc747
Juan Pablo <juan.039@gmail.com>
2022-04-26 15:06:49 -0500

    Remove templates

--- Modulo_2/clase_5/ejercitacion-resuelta-m02c03/app-tareas/funcionesDeTareas.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let archivoTareas = {
-    archivo: 'tareas.json',
-    leerArchivo: function () {
-        return JSON.parse(fs.readFileSync(this.archivo, 'utf-8'));
-    },
-}
-module.exports = archivoTareas;
