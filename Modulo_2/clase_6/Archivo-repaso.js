9c86cc8b2c788aba73209e55331fd6c2f4de8cc2
Juan Pablo <juan.039@gmail.com>
2022-05-03 10:11:09 -0500

    Repaso clase 6

---
+++ Modulo_2/clase_6/Archivo-repaso.js
@@ -0,0 +1,37 @@
+// -------
+// Clase 3
+// -------
+
+// Retomando los arrays
+// Ya vimos lo básico en la clase 2
+// array[0];
+// array.push();
+
+
+// Métodos I - push, pop, shift, unshift, indexOf
+// array.push();
+// array.pop();
+// array.shift(); // a discreción
+// array.unshift(); // a discreción
+// array.indexOf(); // a discreción
+
+
+// Métodos II - forEach, map, filter, reduce
+
+
+// Micro desafío
+// Traer todos los elementos cuya cantidad sea mayor a 1000
+
+array3 = [
+    {cantidad: 21342},
+    {cantidad: 213}, 
+    {cantidad: 742},
+    {cantidad: 7342}
+];
+
+
+// for of -> array
+
+// for in -> objeto literal
+
+
